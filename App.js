433acb2b4eda59a7a7c802951eb7678cdee91b98
DaScientist <37672507+DaScientist@users.noreply.github.com>
2020-03-30 14:05:27 +0530

    Created Drawer

--- App.js
+++ App.js
@@ -10,75 +10,35 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
+  SafeAreaView, StyleSheet, ScrollView,
+  View, Text, StatusBar,
 } from 'react-native';
 
 import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
+  Header, LearnMoreLinks, Colors, DebugInstructions, ReloadInstructions, 
 } from 'react-native/Libraries/NewAppScreen';
 
-import { createStackNavigator } from "@react-navigation/stack";
+// import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import HomeComponent from './Components/HomeComponent.js';
 import SecondComponent from './Components/SecondComponent.js';
 
-const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeComponent} />
-        <Stack.Screen name="Second" component={SecondComponent} />
-      </Stack.Navigator>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeComponent} />
+        <Drawer.Screen name="Second" component={SecondComponent} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
   body: {
     backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
   },
 });
 
