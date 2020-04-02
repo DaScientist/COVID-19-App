fa838c973922bcfe89b235294b08c34fe3cc0675
DaScientist <37672507+DaScientist@users.noreply.github.com>
2020-04-02 19:44:23 +0530

    Renamed Project successfully

--- android/app/src/main/java/com/covid19infoapp/MainActivity.java
+++ android/app/src/main/java/com/covid19infoapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.todolistapp;
+package com.covid19infoapp;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "TodoListApp";
+    return "Covid19InfoApp";
   }
 }
