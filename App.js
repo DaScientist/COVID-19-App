f844a53f306f6a846e6d451048a5b2524df36e32
DaScientist <37672507+DaScientist@users.noreply.github.com>
2020-04-02 23:49:57 +0530

    Transformed renderlinks and formatted App.js

--- App.js
+++ App.js
@@ -6,41 +6,56 @@
  * @flow strict-local
  */
 import 'react-native-gesture-handler'
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import React from 'react'
+import {NavigationContainer} from '@react-navigation/native'
 
 import {
-  SafeAreaView, StyleSheet, ScrollView,
-  View, Text, StatusBar,
-} from 'react-native';
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native'
 
-import {
-  Header, LearnMoreLinks, Colors, DebugInstructions, ReloadInstructions, 
-} from 'react-native/Libraries/NewAppScreen';
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
 // import { createStackNavigator } from "@react-navigation/stack";
-import { createDrawerNavigator } from "@react-navigation/drawer";
-import WHOGuidelinesComponent from './Components/WHOGuidelines/WHOGuidelinesComponent.js';
-import SecondComponent from './Components/SecondComponent.js';
+import {createDrawerNavigator} from '@react-navigation/drawer'
+import WHOGuidelinesComponent from './Components/WHOGuidelines/WHOGuidelinesComponent.js'
+import SecondComponent from './Components/SecondComponent.js'
 
-const Drawer = createDrawerNavigator();
+const Drawer = createDrawerNavigator()
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Infection Prevention and control/WASH" component={WHOGuidelinesComponent} initialParams={{data:'wash'}}/>
-        <Drawer.Screen name="Guidance for schools, workplaces and institutions" component={WHOGuidelinesComponent} initialParams={{data:'institute'}} />
-        <Drawer.Screen name="Points of entry and mass gatherings" component={WHOGuidelinesComponent} initialParams={{data:'mass'}} />
+      <Drawer.Navigator initialRouteName='Home'>
+        
+        <Drawer.Screen
+          name='Infection Prevention and control/WASH'
+          component={WHOGuidelinesComponent}
+          initialParams={{data: 'wash'}} />
+
+        <Drawer.Screen
+          name='Guidance for schools, workplaces and institutions'
+          component={WHOGuidelinesComponent}
+          initialParams={{data: 'institute'}} />
+
+        <Drawer.Screen
+          name='Points of entry and mass gatherings'
+          component={WHOGuidelinesComponent}
+          initialParams={{data: 'mass'}} />
+      
       </Drawer.Navigator>
     </NavigationContainer>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   body: {
     backgroundColor: Colors.white,
   },
-});
+})
 
-export default App;
+export default App
--- Components/WHOGuidelines/GuidelineComponent.js
+++ Components/WHOGuidelines/GuidelineComponent.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{Component} from 'react'
 import {View, Text, Linking, StyleSheet, FlatList} from 'react-native'
 import { DefaultTheme,Button,Card,Title,Paragraph,Colors } from "react-native-paper";
 
@@ -18,25 +18,66 @@ export default function GuidelineComponent ({item,theme}) {
     );
 }
 
-function RenderLinks(props) {
-    // console.log(props.links)
-    return (
-    <FlatList 
-    inverted={false}
-    data={props.links} 
-    collapsable={false}
-    renderItem={({item})=>{
+// export default class GuidelineComponent extends Component {
+//     render(){
+//         return (
+//             <Card>
+//                 <Card.Content>
+//                     <Title style={{fontFamily:'monospace',fontSize:16,fontWeight:"bold",lineHeight:18}}>{this.props.item.title}</Title>
+//                     <Paragraph style={{fontSize:12}}>{this.props.item.content}</Paragraph>
+//                 </Card.Content>
+//                 <Card.Actions>
+//                     <RenderLinks links={this.props.item.links}/>
+//                 </Card.Actions>
+//             </Card>
+//         );  
+//     }
+// }
+
+
+class RenderLinks extends Component {
+    render() {
         return (
-        <Button 
-        mode="contained"
-        color="#0000EEFF"
-        style={{margin:8}}
-        contentStyle={{flex:1,justifyContent:'flex-start',flexWrap:'wrap',}} 
-        onPress={()=>{Linking.openURL(item.link)}}>
-            <Text style={{overflow:'visible',lineHeight:20,fontSize:12,}}>{item.title}</Text>
-        </Button>)
-    }}
-    keyExtractor={item=>item.link}
-    >
-    </FlatList>);
+            <FlatList 
+            inverted={false}
+            data={this.props.links} 
+            collapsable={false}
+            renderItem={({item})=>{
+                return (
+                <Button 
+                mode="contained"
+                color="#0000EEFF"
+                style={{margin:8}}
+                contentStyle={{flex:1,justifyContent:'flex-start',flexWrap:'wrap',}} 
+                onPress={()=>{Linking.openURL(item.link)}}>
+                    <Text style={{overflow:'visible',lineHeight:20,fontSize:12,}}>{item.title}</Text>
+                </Button>)
+            }}
+            keyExtractor={item=>item.link}
+            >
+            </FlatList>);        
+    }
 }
+
+// function RenderLinks(props) {
+//     // console.log(props.links)
+//     return (
+//     <FlatList 
+//     inverted={false}
+//     data={props.links} 
+//     collapsable={false}
+//     renderItem={({item})=>{
+//         return (
+//         <Button 
+//         mode="contained"
+//         color="#0000EEFF"
+//         style={{margin:8}}
+//         contentStyle={{flex:1,justifyContent:'flex-start',flexWrap:'wrap',}} 
+//         onPress={()=>{Linking.openURL(item.link)}}>
+//             <Text style={{overflow:'visible',lineHeight:20,fontSize:12,}}>{item.title}</Text>
+//         </Button>)
+//     }}
+//     keyExtractor={item=>item.link}
+//     >
+//     </FlatList>);
+// }
